f9975b908772878f702836a43fe02287c57faea5
Oscar Munoz <omamr0304@gmail.com>
2025-09-07 12:26:06 -0500

    solving conflicts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,8 +3,4 @@ import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> d3fcf1d6cb13e05bcd9df17c106197b855acb26b
